789eb7d3dffb6a7e7f467ba51e670eab4c235680
LuisRodriguez <luisrodriguezacosta@gmail.com>
2025-04-24 08:43:50 -0500

    rechazar

--- app.vue
+++ app.vue
@@ -1,31 +1,38 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="bg-slate-50">
+    <TopBanner />
+    <Navigation />
+    <MainMenu />
+    <ShoppingCart />
+    <div class="container mx-auto">
+      <PromoSlider />
+      <FeaturedProducts />
+      <BannerMedium />
+      <ProductSlider />
+      <ProductCategories />
+      <ProductsMoreSearched />
+      <SliderWellness />
+      <BannerFinish />
+      <SliderBrandsFeatured />
+      <SliderMarcas class="pb-10" />
+    </div>
+  </div>
+  <Footer class="mt-10"/>
 </template>
 
 <script setup lang="ts">
-useHead({
-  link: [
-    { 
-      rel: 'icon', 
-      type: 'image/png', 
-      href: '/assets/icons/icon_logo.png'
-    }
-  ]
-})
+import TopBanner from '~/components/TopBanner.vue'
+import Navigation from '~/components/Navigation.vue'
+import MainMenu from '~/components/MainMenu.vue'
+import PromoSlider from '~/components/PromoSlider.vue'
+import FeaturedProducts from '~/components/FeaturedProducts.vue'
+import ProductCategories from '~/components/ProductCategories.vue'
+import ProductSlider from '~/components/ProductSlider.vue'
+import BannerMedium from '~/components/BannerMedium.vue'
+import Footer from '~/components/Footer.vue'
+import BannerFinish from './components/BannerFinish.vue'
+import SliderMarcas from './components/SliderMarcas.vue'
+import SliderWellness from './components/SliderWellness.vue'
+import SliderBrandsFeatured from './components/SliderBrandsFeatured.vue'
+import ShoppingCart from '~/components/ShoppingCart.vue'
 </script>
-
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Inter', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-* {
-  box-sizing: border-box;
-}
-</style>
